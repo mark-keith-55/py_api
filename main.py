f717f7b7e9aa5862d9746315c054a111593fbd8e
kesmaru <kesamaru.bizz@gmail.com>
2025-06-06 15:07:33 +0900

    main.pyを追加

---
+++ main.py
@@ -0,0 +1,16 @@
+ from dotenv import load_dotenv
+import google.generativeai as genai
+ 
+ # .envファイルの読み込み
+ load_dotenv()
+ 
+ # API-KEYの設定
+ GOOGLE_API_KEY=dotenv('GOOGLE_API_KEY')
+ genai.configure(api_key=GOOGLE_API_KEY)
+ 
+ gemini_pro = genai.GenerativeModel("gemini-pro")
+ prompt = "こんにちは"
+ response = gemini_pro.generate_content(prompt)
+ print(response.text)
+ 
+ 
